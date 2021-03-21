5fc5e91e70a35dc12d43d6a7a369ab0cd2fb521b
Isura Dilshan <isuradilshan.idw@gmail.com>
2021-03-21 17:07:43 +0530

    mobileapp update

--- fivetagsmobileapp/lib/splash.dart
+++ fivetagsmobileapp/lib/splash.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:fivetagsmobileapp/constant.dart';
 
+import 'constant.dart';
+import 'constant.dart';
+import 'constant.dart';
+
 class Splash extends StatefulWidget {
   @override
   _SplashState createState() => _SplashState();
@@ -22,24 +26,47 @@ class _SplashState extends State<Splash> {
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
+                padding: const EdgeInsets.only(top: 100.0),
                 child: Image.asset(
                   'images/logo.png',
-                  height: 200.0,
+                  height: 140.0,
                 ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 20.0, top: 70.0),
+                child: Text(
+                  'Welcom to Our',
+                  style: TextStyle(
+                    fontSize: 23.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 7.0,
               ),
               Center(
                 child: Container(
                   child: Text(
-                    'SkyLoft',
+                    'SkyLight Complex',
                     style: TextStyle(
-                      fontSize: 50.0,
+                      fontSize: 48.0,
                       fontWeight: FontWeight.w400,
                       color: Colors.white,
                     ),
                   ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 200.0),
+                child: Text(
+                  'User Application',
+                  style: TextStyle(
+                      fontSize: 22.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400),
                 ),
               ),
             ],
