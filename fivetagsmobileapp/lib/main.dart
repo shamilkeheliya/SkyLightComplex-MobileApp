8a8a4912bdd6a7ba83431b141087c1b08e4f60fe
Isura Dilshan <isuradilshan.idw@gmail.com>
2021-03-22 01:14:04 +0530

    up

--- fivetagsmobileapp/lib/main.dart
+++ fivetagsmobileapp/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //return Splash();
-    return Login();
-    //return Home();
+    // return Login();
+    return Home();
   }
 }
