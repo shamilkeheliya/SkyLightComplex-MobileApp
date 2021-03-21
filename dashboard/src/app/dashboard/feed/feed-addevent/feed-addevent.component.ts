db59fe3f675c74787e19a6d69eb48f9da795a5c6
shamilkeheliya <shamilkeheliya@users.noreply.github.com>
2021-03-21 17:12:51 +0530

    updated

--- dashboard/src/app/dashboard/feed/feed-addevent/feed-addevent.component.ts
+++ dashboard/src/app/dashboard/feed/feed-addevent/feed-addevent.component.ts
@@ -6,7 +6,7 @@ import { FormBuilder,FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Component,OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
-//import { Finalize } from 'rxjs/operators';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-feed-addevent',
@@ -14,7 +14,8 @@ import { AngularFireStorage } from '@angular/fire/storage';
   styleUrls: ['./feed-addevent.component.css']
 })
 export class FeedAddeventComponent implements OnInit {
-
+uploadPercent:Observable<number>
+downloadURL:Observable<string>
   form = this.fb.group({
     eventTitle: new FormControl('',[Validators.required]),
     eventDescription: new FormControl('',[Validators.required]),
@@ -25,48 +26,48 @@ export class FeedAddeventComponent implements OnInit {
   constructor(
     private afs: AngularFirestore,
     private snack: MatSnackBar,
-    private fireauth: AngularFireAuth,
+    // private fireauth: AngularFireAuth,
     private storage: AngularFireStorage,
     private fb: FormBuilder,
-    private fireStore: AngularFirestore,) { }
+    ) { }
 
   ngOnInit(): void {
   }
 
-  // addEvent() {
-  //   if (this.form.valid) {
-  //     if (this.downloadURL) {
-  //       this.downloadURL.subscribe(url=>{
-  //         this.afs.collection("doa_stor").add({
-  //           prodName: this.form.value.prodName,
-  //           subDis: this.form.value.subDis,
-  //           mainDis: this.form.value.mainDis,
-  //           price: this.form.value.price,
-  //           img: url
-  //         }).then(() => {
-  //           this.snack.open("Added success", "ok");
-  //         })
-  //       })
-  //     }
-  //   }
-  // }
+  addEvent() {
+    if (this.form.valid) {
+      if (this.downloadURL) {
+        this.downloadURL.subscribe(url=>{
+          this.afs.collection('events').add({
+            title: this.form.value.eventTitle,
+            description: this.form.value.eventDescription,
+            date: this.form.value.eventDate,
+            time: this.form.value.eventTime,
+            imageURL: url
+          }).then(() => {
+            this.snack.open("Added success", "ok");
+          })
+        })
+      }
+    }
+  }
   
-  // uploadFile(event) {
+  uploadFile(event) {
   
-  //   let name = new Date();
-  //   const file = event.target.files[0];
-  //   const filePath = name.getTime().toString();
-  //   const fileRef = this.storage.ref(filePath);
-  //   const task = this.storage.upload(filePath, file);
-
-  //   // observe percentage changes
-  //   this.uploadPercent = task.percentageChanges();
-  //   // get notified when the download URL is available
-  //   task.snapshotChanges().pipe(
-  //     finalize(() => this.downloadURL = fileRef.getDownloadURL())
-  //   )
-  //     .subscribe()
-  // }
-addEvent(){}
+    let name = new Date();
+    const file = event.target.files[0];
+    const filePath = name.getTime().toString();
+    let n = ("eventImages/"+filePath);
+    const fileRef = this.storage.ref(n);
+    const task = this.storage.upload(n, file);
+    // observe percentage changes
+    this.uploadPercent = task.percentageChanges();
+    // get notified when the download URL is available
+    task.snapshotChanges().pipe(
+      finalize(() => this.downloadURL = fileRef.getDownloadURL())
+    )
+      .subscribe()
+  }
+//addEvent(){}
   
 }
