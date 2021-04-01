b0592c7704e51f48f47b39a1ca206fc38804e6ac
shamilkeheliya <shamilkeheliya@users.noreply.github.com>
2021-04-01 15:48:30 +0530

    updated

--- fivetagsmobileapp/lib/UI/navBar/feed.dart
+++ fivetagsmobileapp/lib/UI/navBar/feed.dart
@@ -19,49 +19,81 @@ class _HomeState extends State<Feed> {
     print(user);
   }
 
+  final _kTabPages = <Widget>[
+    Center(child: Genaral()),
+    Center(child: Icon(Icons.alarm, size: 64.0, color: Colors.cyan)),
+  ];
+  final _kTabs = <Tab>[
+    const Tab(text: 'Genaral'),
+    const Tab(text: 'Special'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(
-          color: blueMid,
-        ),
-        backgroundColor: blueLight,
-        title: const Text(
-          'SkyLight NewsFeed',
-          style: TextStyle(
-            fontFamily: mainFont,
+    return DefaultTabController(
+      length: _kTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            indicatorColor: blueDark,
+            labelColor: blueDark,
+            unselectedLabelColor: blueMid,
+            tabs: _kTabs,
+          ),
+          iconTheme: IconThemeData(
             color: blueMid,
           ),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.notifications),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return Notifications();
-                  },
-                ),
-              );
-            },
+          backgroundColor: blueLight,
+          title: const Text(
+            'SkyLight NewsFeed',
+            style: TextStyle(
+              fontFamily: mainFont,
+              color: blueMid,
+            ),
           ),
-        ],
-      ),
-      drawer: Drwr(),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment(0.0, -1.0),
-            end: Alignment(0.0, 1.0),
-            colors: [blueLight, blueDark],
-            stops: [0.0, 1.0],
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.notifications),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Notifications();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+        drawer: Drwr(),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(0.0, -1.0),
+              end: Alignment(0.0, 1.0),
+              colors: [blueLight, blueDark],
+              stops: [0.0, 1.0],
+            ),
+          ),
+          child: TabBarView(
+            children: _kTabPages,
           ),
         ),
+        bottomNavigationBar: NavBar(),
       ),
-      bottomNavigationBar: NavBar(),
     );
   }
+}
+
+Column Genaral() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: [
+      Container(
+        child: Text('shmail'),
+      ),
+    ],
+  );
 }
