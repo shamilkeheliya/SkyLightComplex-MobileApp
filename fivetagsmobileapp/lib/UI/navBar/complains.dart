bb7841a03c7be7a617df8851edbcf22b4c6576be
shamilkeheliya <shamilkeheliya@users.noreply.github.com>
2021-04-01 19:45:33 +0530

    complains UI done

--- fivetagsmobileapp/lib/UI/navBar/complains.dart
+++ fivetagsmobileapp/lib/UI/navBar/complains.dart
@@ -1,4 +1,5 @@
 import 'package:fivetagsmobileapp/UI/notifications.dart';
+import 'package:fivetagsmobileapp/UI/requests/laborRequests/plumber.dart';
 import 'package:fivetagsmobileapp/navBar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -52,8 +53,70 @@ class _HomeState extends State<Complains> {
             stops: [0.0, 1.0],
           ),
         ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            btn(Icons.bolt, 'Electrical Issue', Plumber()),
+            btn(Icons.water_damage_outlined, 'Water Issue', Plumber()),
+            btn(Icons.people, 'Annoying Neighbor', Plumber()),
+            btn(Icons.report, 'Other', Plumber()),
+          ],
+        ),
       ),
       bottomNavigationBar: NavBar(),
+    );
+  }
+
+  Expanded btn(icon, title, page) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 15.0),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            gradient: LinearGradient(
+              begin: Alignment(0.0, -1.0),
+              end: Alignment(0.0, 1.0),
+              colors: [blueLightUnelected, blueLightSelected],
+              stops: [0.0, 1.0],
+            ),
+          ),
+          child: FlatButton(
+            //color: blueLightSelected,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return page;
+                  },
+                ),
+              );
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Icon(
+                    icon,
+                    color: blueDark,
+                    size: 50.0,
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                        fontSize: 25.0,
+                        color: blueDark,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
