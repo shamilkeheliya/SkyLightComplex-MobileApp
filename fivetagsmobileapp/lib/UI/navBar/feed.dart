5c339fc3b6d307b33ae738653c8452d60abbaf77
shamilkeheliya <shamilkeheliya@users.noreply.github.com>
2021-04-11 02:30:21 +0530

    First time Password change Done

--- fivetagsmobileapp/lib/UI/navBar/feed.dart
+++ fivetagsmobileapp/lib/UI/navBar/feed.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fivetagsmobileapp/services/eventsService.dart';
 import 'package:fivetagsmobileapp/services/genaralService.dart';
 import 'package:fivetagsmobileapp/services/yourReservations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fivetagsmobileapp/constant.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Feed extends StatefulWidget {
   @override
@@ -24,8 +26,11 @@ class _HomeState extends State<Feed> {
     const Tab(text: 'My Reservations'),
   ];
 
+  final keyIsFirstLoaded = 'is_first_loaded';
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration.zero, () => showDialogIfFirstLoaded(context));
+
     return DefaultTabController(
       length: _kTabs.length,
       child: Scaffold(
@@ -57,5 +62,81 @@ class _HomeState extends State<Feed> {
         ),
       ),
     );
+  }
+//}
+
+  showDialogIfFirstLoaded(BuildContext context) async {
+    String pwd;
+    final _pwd = TextEditingController();
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isFirstLoaded = prefs.getBool(keyIsFirstLoaded);
+    if (isFirstLoaded == null) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          // return object of type Dialog
+          return AlertDialog(
+            backgroundColor: blueLightSelected,
+            title: new Text("Change Password"),
+            content: Container(
+              child: TextField(
+                obscureText: true,
+                controller: _pwd,
+                style: TextStyle(
+                  color: blueDark,
+                ),
+                onChanged: (value) {
+                  pwd = value;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'New Password',
+                  labelStyle: TextStyle(color: blueDark),
+                ),
+              ),
+            ),
+            actions: <Widget>[
+              // usually buttons at the bottom of the dialog
+              new FlatButton(
+                child: new Text(
+                  "Change Password",
+                  style: TextStyle(color: blueDark),
+                ),
+                onPressed: () {
+                  if (_pwd.text.isEmpty == null) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Please enter New Password'),
+                        action: SnackBarAction(
+                          label: 'OK',
+                          onPressed: () {},
+                        ),
+                      ),
+                    );
+                  } else {
+                    final _auth = FirebaseAuth.instance;
+                    final User user = _auth.currentUser;
+                    user.updatePassword(pwd).then((value) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Password Updated Successfully'),
+                          action: SnackBarAction(
+                            label: 'OK',
+                            onPressed: () {},
+                          ),
+                        ),
+                      );
+                      Navigator.pop(context);
+                      prefs.setBool(keyIsFirstLoaded, false);
+                    });
+                  }
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 }
